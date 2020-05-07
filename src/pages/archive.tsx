edec8175c591311f1fa6a99b14613a911a8154d2
nbamford123 <54650915+nbamford123@users.noreply.github.com>
2020-05-06 20:38:04 -0700

    Moar archive (#6)
    * adding dropdown for go to page select
    
    * archive jump to page

--- src/pages/archive.tsx
+++ src/pages/archive.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { css } from '@emotion/core';
 import Image from 'gatsby-image';
-import { Link } from 'gatsby';
+import { Link, navigate } from 'gatsby';
+import Select from 'react-select';
 
 import Layout from '../components/layout';
 import { chapterSummary } from '../hooks/chapterSummary';
 
+// TODO: Fix page indexing when chapter title pages (0) are added
 const Archive: React.FC<{}> = () => {
   const chapters = chapterSummary();
   return (
@@ -49,13 +51,36 @@ const Archive: React.FC<{}> = () => {
             </Link>
             <div
               css={css`
+                display: flex;
+                flex-direction: column;
                 flex: 1 1 auto;
+                height: 40px;
               `}
             >
               <h2>
                 Chapter {chapter.chapter} {chapter.title}
               </h2>
               <p>{chapter.synopsis}</p>
+              <Select
+                css={css`
+                  margin-top: 1rem;
+                  * {
+                    margin-top: 0;
+                  }
+                `}
+                placeholder="Jump to page"
+                onChange={(value, action) =>
+                  action.action === 'select-option' &&
+                  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                  navigate((value as any).value)
+                }
+                options={[{ value: '', label: 'Go to page' }].concat(
+                  Array.from(Array(chapter.pages), (_, index) => ({
+                    value: `${chapter.chapter}-${index + 1}`,
+                    label: `${index + 1}`,
+                  })),
+                )}
+              />
             </div>
           </div>
         ))}
