c141dd32378d85822e2fb908ed061c827c5e0e88
nate <nathan.bamford@gmail.com>
2021-08-25 23:35:09 -0700

    animation and sounds

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { css } from '@emotion/react';
 import { Helmet } from 'react-helmet';
 import {
+  Appear,
   Arwes,
   Col,
   // createResponsive,
+  createSounds,
   createTheme,
   Frame,
   Row,
+  SoundsProvider,
   ThemeProvider,
 } from 'arwes';
 
@@ -27,51 +30,85 @@ const myTheme = {
   },
 };
 const background = {
-  small: './img/background.jpg',
-  medium: './img/background-medium.jpg',
-  large: './img/background-large.jpg',
-  xlarge: './img/background-large.jpg',
+  small: '/img/background.jpg',
+  medium: '/img/background-medium.jpg',
+  large: '/img/background-large.jpg',
+  xlarge: '/img/background-large.jpg',
 };
 
 // const responsive = createResponsive({
 //   getTheme: () => myTheme,
 // });
 
+const sounds = {
+  shared: { volume: 1 },
+  players: {
+    click: {
+      sound: { src: ['sound/click.mp3'] },
+      settings: { oneAtATime: true },
+    },
+    typing: {
+      sound: { src: ['sound/typing.mp3'] },
+      settings: { oneAtATime: true },
+    },
+    deploy: {
+      sound: { src: ['sound/deploy.mp3'] },
+      settings: { oneAtATime: true },
+    },
+  },
+};
+
 const Layout: React.FC<LayoutProps> = ({ children, page }) => {
   const { title, description } = useSiteMetadata();
 
   return (
     <ThemeProvider theme={createTheme(myTheme)}>
-      <Arwes animate pattern="./img/glow.png" background={background}>
-        <Helmet>
-          <html lang="en" />
-          <title>{title}</title>
-          <meta name="description" content={description} />
-        </Helmet>
-        <Header />
-        <div
-          css={css`
-            margin-left: auto;
-            margin-right: auto;
-            margin-top: 1rem;
-            max-width: ${TOTAL_WIDTH}px;
-          `}
-        >
-          <Row>
-            <Col s={12} xl={9}>
-              <Frame animate level={1} corners={3}>
-                {page}
-              </Frame>
-            </Col>
-            <Col s={12} xl={3}>
-              <Sidebar />
-            </Col>
-          </Row>
-          <Row>
-            <Col s={12}>{children}</Col>
-          </Row>
-        </div>
-      </Arwes>
+      <SoundsProvider sounds={createSounds(sounds)}>
+        <Arwes animate pattern="/img/glow.png" background={background}>
+          {(anim: { entered: boolean }) => (
+            <>
+              <Helmet>
+                <html lang="en" />
+                <title>{title}</title>
+                <meta name="description" content={description} />
+              </Helmet>
+              <Header show={anim.entered} />
+              <div
+                css={css`
+                  margin-left: auto;
+                  margin-right: auto;
+                  margin-top: 1rem;
+                  max-width: ${TOTAL_WIDTH}px;
+                `}
+              >
+                <Row>
+                  <Col s={12} xl={9}>
+                    <Frame animate level={1} corners={3}>
+                      <div
+                        css={css`
+                          display: flex;
+                          flex-direction: column;
+                          align-items: center;
+                        `}
+                      >
+                        <Appear animate show={anim.entered}>
+                          {page}
+                        </Appear>
+                      </div>
+                    </Frame>
+                  </Col>
+                  <Col s={12} xl={3}>
+                    <Sidebar />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col s={12}>{children}</Col>
+                </Row>
+              </div>
+            </>
+          )}
+        </Arwes>
+      </SoundsProvider>
     </ThemeProvider>
   );
 };
--- src/util/getComicPath.js
+++ src/util/getComicPath.js
@@ -1,4 +1,4 @@
 // The relatvie URL of the given comic
 module.exports = {
-  getComicPath: (chapter, page) => `/comics/${chapter}/${page}`,
+  getComicPath: (chapter, page) => `comics/${chapter}/${page}`,
 };
