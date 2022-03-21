b10268cf972726051e8288e37ec2178af34f404b
nate <nathan.bamford@gmail.com>
2022-03-20 19:56:59 -0700

    beating mobile menu into shape

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,9 @@
-import {
-  AiOutlineCalendar,
-  AiOutlineFileText,
-  AiOutlineFolderOpen,
-  AiOutlineQuestionCircle,
-} from 'react-icons/ai';
-import { FiMenu } from 'react-icons/fi';
 import { css } from '@emotion/react';
-import { Appear, Header as ArwesHeader, Highlight, Words } from 'arwes';
+import { Header as ArwesHeader, Words } from 'arwes';
 import { Link } from 'gatsby';
 
-import { ArwesIcon } from './arwesIconStyles';
-import { NavLink } from './navLink';
+import { Menu } from './menu';
+import { MobileMenu } from './mobileMenu';
 import { SolarSalvageTitle } from './title';
 import { TOTAL_WIDTH } from '../types';
 
@@ -26,38 +19,12 @@ const iconCss = css`
   position: relative;
   top: 2px;
 `;
-const HeaderNavLink: React.FC<{ show: boolean; text: string; to: string }> = ({
-  show,
-  children,
-  text,
-  to,
-}) => (
-  <NavLink header to={to}>
-    <Highlight animate layer="header">
-      <Appear animate show={show}>
-        {children}
-      </Appear>
-      <Words
-        css={css`
-          margin-left: 0.5rem;
-        `}
-        animate
-        show={show}
-      >
-        {text}
-      </Words>
-    </Highlight>
-  </NavLink>
-);
-
 const HeaderNavMenu: React.FC<{ mobile: boolean; entered: boolean }> = ({
   entered,
   mobile = false,
 }) =>
   mobile ? (
-      <ArwesIcon>
-        <FiMenu css={iconCss} />
-      </ArwesIcon>
+    <MobileMenu entered={entered}/>
   ) : (
     <div
       css={css`
@@ -65,18 +32,7 @@ const HeaderNavMenu: React.FC<{ mobile: boolean; entered: boolean }> = ({
         display: flex;
       `}
     >
-      <HeaderNavLink show={entered} text="LATEST PAGE" to="/">
-        <AiOutlineCalendar css={iconCss} />
-      </HeaderNavLink>
-      <HeaderNavLink show={entered} text="NEWS" to="/blog">
-        <AiOutlineFileText css={iconCss} />
-      </HeaderNavLink>
-      <HeaderNavLink show={entered} text="ABOUT" to="/about">
-        <AiOutlineQuestionCircle css={iconCss} />
-      </HeaderNavLink>
-      <HeaderNavLink show={entered} text="ARCHIVE" to="/archive">
-        <AiOutlineFolderOpen css={iconCss} />
-      </HeaderNavLink>
+      < Menu entered={entered}/>
     </div>
   );
 
@@ -104,7 +60,7 @@ export const Header: React.FC<{ show: boolean }> = ({ show }) => (
             <SolarSalvageTitle />
           </Link>
           <nav>
-            <HeaderNavMenu entered={headerAnim.entered} mobile={true} />
+            <HeaderNavMenu entered={headerAnim.entered} mobile={false} />
           </nav>
         </div>
         <div
